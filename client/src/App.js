c54f110ca3f835198ef3ee4a3cbff6ba58197c77
Eric Iwan <eric.iwan@mail.com>
2023-09-28 07:19:34 +0000

    Lots of updates

--- client/src/App.js
+++ client/src/App.js
@@ -2,23 +2,29 @@ import "./styles.css";
 
 import { useState, useEffect } from "react";
 
-function CategoryAmount({ parentCallback, idval }) {
+function CategoryAmount({ parentCallback, idval, val }) {
   return (
     <input
       className="categorybox"
       placeholder="Budgeted"
       id="categoryamount"
+      value={val}
       onChange={(event) => parentCallback(event, idval)}
     />
   );
 }
 
-function CategoryName({ categname, idval }) {
+function CategoryName({ categname, idval, val }) {
   return (
-    <input
-      placeholder="Category"
-      onChange={(event) => categname(event, idval)}
-    />
+    <div  className="category">
+      <input type="checkbox" id={idval} value="savings" className="checkbox" />
+      <input
+        placeholder="Category"
+        className="category-name"
+        value={val[1]}
+        onChange={(event) => categname(event, idval)}
+      />
+    </div>
   );
 }
 
@@ -36,7 +42,7 @@ function AmountBox({ Numvalue, Spent }) {
 }
 
 function NewBox({ handleClick, title }) {
-  return <button onClick={handleClick}>{title}</button>;
+  return <button className="newbutton" onClick={handleClick}>{title}</button>;
 }
 
 function TransCat({ categories, change, index }) {
@@ -90,25 +96,38 @@ function Row({
 }
 
 export default function App() {
-  //boxvlue = [category, budgetamount, spent]
-  const [boxvalue, setBoxvalue] = useState(Array(3).fill(["", 0, 0]));
+  //boxvlue = [id, category, budgetamount, spent]
+  const [boxvalue, setBoxvalue] = useState(Array(3).fill([]));
   const [total, setTotal] = useState(0);
   //transaction = [name, category, expense, income]
   const [transaction, setTransaction] = useState(Array(5).fill(["", "", 0, 0]));
   //Backend
   const [backend, setBackend] = useState({});
 
+  useEffect(() => {
+    fetch("/load")
+    .then(response => response.json())
+    .then(data => {
+      let stuff = boxvalue.slice()
+      console.log("Slice", stuff)
+      for(let i in data){
+        stuff[i] = [data[i]._id, data[i].name, data[i].amount, data[i].spent] ;
+      }
+      setBoxvalue(stuff)
+    })
+  }, [])
 
   useEffect(() => {
-    fetch("/api")
+    console.log("box", boxvalue)
+    fetch("/update", {method: "POST", headers: {"Content-Type": "application/json"}, body: JSON.stringify(boxvalue)})
     .then(response=> response.json())
     .then(
       data=> {
-        setBackend(data)
+        console.log("/update response data: ", data)
       }
     )
-  }, []);
-console.log(backend)
+  }, boxvalue);
+
 
   function modifyNum(arr, filterArr) {
     for (let j = 0; j < arr.length; j++) {
@@ -174,11 +193,12 @@ console.log(backend)
           event.target.value = "¥" + str;
           nextBoxVal[id] = [
             nextBoxVal[id][0],
+            nextBoxVal[id][1],
             parseFloat(boxstr),
-            nextBoxVal[id][2]
+            nextBoxVal[id][3]
           ];
         }
-        tot += nextBoxVal[i][1];
+        tot += nextBoxVal[i][2];
       }
       setBoxvalue(nextBoxVal);
       setTotal(tot);
@@ -188,18 +208,18 @@ console.log(backend)
       let spent = 0;
       for (let ii = 0; ii < nextTransaction.length; ii++) {
         if (
-          nextBoxVal[x][0] === nextTransaction[ii][1] &&
+          nextBoxVal[x][1] === nextTransaction[ii][1] &&
           nextTransaction[ii][2] > 0
         ) {
           spent += nextTransaction[ii][2];
         } else if (
-          nextBoxVal[x][0] === nextTransaction[ii][1] &&
+          nextBoxVal[x][1] === nextTransaction[ii][1] &&
           nextTransaction[ii][3] > 0
         ) {
           spent -= nextTransaction[ii][3];
         }
       }
-      nextBoxVal[x] = [nextBoxVal[x][0], nextBoxVal[x][1], spent];
+      nextBoxVal[x] = [nextBoxVal[x][0], nextBoxVal[x][1], nextBoxVal[x][2], spent];
       setBoxvalue(nextBoxVal);
     }
   }
@@ -210,7 +230,7 @@ console.log(backend)
     const nextBox = boxvalue.slice();
     for (let i = 0; i <= nextBox.length; i++) {
       if (i === id) {
-        nextBox[i] = [event.target.value, nextBox[i][1], nextBox[i][2]];
+        nextBox[i] = [id, event.target.value, nextBox[i][2], nextBox[i][3]];
       }
     }
     setBoxvalue(nextBox);
@@ -242,25 +262,15 @@ console.log(backend)
     setTransaction(tempTrans);
   }
 
-  /* LEFTOVER FROM
-  function remainder(event, index) {
-    const newTrans = transaction.slice();
-    const arr = newTrans[index][1].toString().slice();
-    const filterArr = [];
-    const boxstr = modifyNum(arr, filterArr);
-    const str = boxstr.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    return "¥" + str;
-  }
 
-  */
 
   return (
     <div className="App">
       <h1> Budget thingy </h1>
       <div className="budget">
-        <div className="category">
+        <div className="categorydiv">
           {boxvalue.map((value, index) => (
-            <CategoryName key={index} idval={index} categname={handleCatName} />
+            <CategoryName key={index} idval={index} val={value} categname={handleCatName} />
           ))}
         </div>
         <div className="categoryamount" id="inputamount">
@@ -268,6 +278,7 @@ console.log(backend)
             <CategoryAmount
               key={index}
               idval={index}
+              val = {value[2]}
               parentCallback={(x, y) => handleInput(x, y)}
             />
           ))}
@@ -277,20 +288,25 @@ console.log(backend)
             <AmountBox
               key={index}
               idval={index}
-              Numvalue={value[1]}
-              Spent={value[2]}
+              Numvalue={value[2]}
+              Spent={value[3]}
             />
           ))}
         </div>
-        <div className="newbutton">
+        <div className="newcat">
           <NewBox
             title="New Category"
-            handleClick={() => setBoxvalue([...boxvalue, ["", 0]])}
+            handleClick={() => setBoxvalue([...boxvalue, [0, "", 0, 0]])}
           />
         </div>
         <div className="total-amount" id="total">
           <p>Total: ¥{total.toLocaleString()}</p>
         </div>
+      </div>
+      <div className="newrow">
+        <button className="newbutton" onClick={() => newRow()}>
+          New Row
+        </button>
       </div>
       <div className="transactions">
         {transaction.map((event, index) => (
@@ -306,11 +322,7 @@ console.log(backend)
         ))}
       </div>
 
-      <div className="newbutton">
-        <button className="newrow" onClick={() => newRow()}>
-          New Row
-        </button>
-      </div>
+      
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,55 @@
 const express = require("express");
+require("dotenv").config();
+const {MongoClient} = require("mongodb");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
 
 const app = express();
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
 
-app.get("/api", (req,res) => {
-    res.send({"What": "Howdy there"})
-})
+
+const url = process.env.MONGO_URI;
+
+const db = mongoose.createConnection(url, {useNewUrlParser: true, useUnifiedTopology: true})
+
+var categorySchema = new mongoose.Schema({_id: Number, name: String, amount: Number, spent: Number})
+let Category = db.model("Category", categorySchema);
+
+
+//MIBHG NEED TO MAKE DOCUMENTS IN THE boxvalue BEFORE IT CAN UPDATE.
+
+
+
+app.get("/load", async (req, res) => {
+  try {
+    const data = await Category.find({}).exec();
+    if (data && data.length > 0) {
+      console.log(data);
+      res.json(data);
+    } else {
+      console.log("No data found.");
+      res.status(404).json({ message: "No data found" });
+    }
+  } catch (err) {
+    console.error("ERR:", err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+
+app.post("/update", async (req, res) => {
+  for(let i in req.body){
+    Category.findByIdAndUpdate({_id: req.body[i][0]}, {name: req.body[i][1], amount: req.body[i][2], spent: req.body[i][3]}, {new:true})
+  } 
+  
+});
 
 app.listen(5000, () => {
     console.log("Server running!")
-})
+});
